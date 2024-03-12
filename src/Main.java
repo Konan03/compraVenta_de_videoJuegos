c5aa0bf5de2742d0464851742fd9f221072c3608
Juan David <juandavidgonzalez1@hotmail.com>
2024-03-12 11:11:09 -0500

    The class slingleton was add

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 import controller.ControllerVideoJuego;
 import view.GUIPrincipal;
-
-
+import model.EmpresaVideojuego;
 import static controller.ControllerVideoJuego.agregarVideoJuego;
 
 public class Main {
     public static void main(String[] args) {
 
-        ControllerVideoJuego controllerVideoJuego = new ControllerVideoJuego();
 
+        EmpresaVideojuego emp1 = EmpresaVideojuego.getEmpresa();
+        ControllerVideoJuego controllerVideoJuego = new ControllerVideoJuego();
         GUIPrincipal guiPrincipal = new GUIPrincipal();
         guiPrincipal.setVideojuego(controllerVideoJuego);
         guiPrincipal.setVisible(true);
